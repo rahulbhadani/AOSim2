ae8ca9bbecee15d59268ddc6971d04fd1034672d
Johanan L. Codona <jlcodona@gmail.com>
2015-02-17 07:47:08 -0700

    Cleaning up.

--- examples/jlc_example1.m
+++
@@ -1,6 +0,0 @@
-%I was going to walk through making an AOSegment and just using it as a simple pupil.  Talk about wrapping it as an AOAperture, but not doing anything fancy with that unless they want to have a multi-segment pupil that uses those features.  In the simple case of one segment you can just use AOSegment. Then I was going to make an F=AOField(A) and show how it is just as big as A.  Since we really want to allow propagated fields to be bigger than A, I will show how to define it to be bigger.
-%I am going to talk about my rather primitive pupil specification matrix and how to just load a grid from a paint program.  
-%I was going to show how to change the size of the grid and the spacing.  
-%Then I am going to set a wavelength and do some simple diffraction experiments.  
-%I will show how to set F.FFTSize and what it means. 
-
