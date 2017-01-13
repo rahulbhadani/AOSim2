a3bdd682d38941903937d4ae6ecc36ea7d697d74
Johanan L. Codona <jlcodona@gmail.com>
2017-01-12 17:10:33 -0700

    Added debugging plots to AOField.propagateThrough. Turn on using ATMO.verbosity>0.

--- AOSim2/@AOField/propagateThrough.m
+++ AOSim2/@AOField/propagateThrough.m
@@ -18,9 +18,23 @@ while(F.z>FinalZ)
     if(isempty(ZnextIndex))
         break;
     end
+    if(ATMO.verbosity>0)
+        fprintf('AOField.propagateThrough z is now %.0f\n',F.z);
+    end
 
     F.propagate2(F.z-Zlist(ZnextIndex))*ATMO.layers{ZnextIndex}.screen; % Fix WIND!!!
 
+    if(ATMO.verbosity>0)
+        clf;
+        subplot(2,2,1);
+        D = 1; %mean(F.extent);
+        [x,y] = ATMO.layers{ZnextIndex}.screen.coords;
+        imagesc(x,y,ATMO.layers{ZnextIndex}.screen.grid,[-1 1]*2e-6);
+        axis xy; sqar; colorbar;
+        setFoV(D);
+        title(sprintf('phase screen %d',ZnextIndex));
+    end
+    
     if(isa(ATMO,'AOAtmo2'))
         F*ATMO.layers{ZnextIndex}.shadow; % Fix WIND!!!
         if(~isempty(ATMO.shadowUpdate))
@@ -28,6 +42,31 @@ while(F.z>FinalZ)
                 error('Error updating AOAtmo2 layer shadow.');
             end
         end
+        
+        if(ATMO.verbosity>0)
+            subplot(2,2,2);
+            %ATMO.layers{ZnextIndex}.shadow.show; setFoV(D);
+            [x,y] = ATMO.layers{ZnextIndex}.shadow.coords;
+            imagesc(x,y,ATMO.layers{ZnextIndex}.shadow.grid,[0 1]*1e-6);
+            axis xy; sqar; colorbar;
+            title(sprintf('shadow screen %d',ZnextIndex));
+        end
+    end
+
+    if(ATMO.verbosity>0)
+        subplot(2,2,3);
+        F.plotC(4);
+        setFoV(D);
+        title('Complex Field');
+        
+        subplot(2,2,4);
+        F.plotDex([-3 0]+3);
+        setFoV(D);
+        title('log Irradiance');
+        
+        drawnow;
+        
+        savePNG(sprintf('%d_atmo2_%.0f.png',ZnextIndex,ATMO.time*1e3),150);
     end
     
     F.z = F.z - eps;
