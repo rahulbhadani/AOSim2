0aaeaab227cd496cf715fcff408a1c40dfd4e834
Johanan L. Codona <jlcodona@gmail.com>
2017-01-17 09:02:34 -0700

    Made GPUdemo1 less dependent on graphics.

--- AOSim2/demos/GPUdemo1.m
+++ AOSim2/demos/GPUdemo1.m
@@ -1,54 +1,40 @@
+Nloops = 5;
+N = 250;
+GFX = false;
+
+gpu = gpuDevice(1);
+gpu.reset;
+
 PS = AOScreen(512);
 PS.name = 'Phase Screen';
 PS.describe;
+CUBE = zeros([512 512 N]);
 
 [X,Y] = PS.COORDS;
 colormap(gray(256));
 
-for GFX=[false true]
-    PS.gpuify(false);
-    PS.describe
+for USE_GPU=[false true]
+    PS.grid(single(PS.grid_));
+    PS.gpuify(USE_GPU);
     
-    for m=1:5
+    for m=1:Nloops
         tic;
         PS.make;
-        fprintf('Loop %d: %s make took %.4f secs\n',m,PS.describe,toc);
+        fprintf('Loop %d: %s make took %.4f secs, ',m,PS.describe,toc);
         
         tic;
         s=1;
-        for n=1:250
+        for n=1:N
             s = s * .99;
-            G = PS.interpGrid(s*X,s*Y);
-            if(GFX)
-                imagesc([X(1) X(end)]*s,[Y(1) Y(end)]*s,G);
-                sqar; axis xy;
-                title(PS.describe);
-                drawnow;
-            end
+            CUBE(:,:,n) = gather(PS.interpGrid(s*X,s*Y));
         end
-        fprintf('Just Zooming took %.4f secs\n',toc);
-    end
-    
-    PS.gpuify(true);
-    PS.describe
-    
-    for m=1:5
-        tic;
-        PS.make;
-        fprintf('Loop %d: %s make took %.4f secs\n',m,PS.describe,toc);
+        fprintf('Zooming took %.4f secs.\n',toc);
         
-        tic;
-        s=1;
-        for n=1:250
-            s = s * .99;
-            G = PS.interpGrid(s*X,s*Y);
-            if(GFX)
-                imagesc([X(1) X(end)]*s,[Y(1) Y(end)]*s,G);
-                sqar; axis xy;
-                title(PS.describe);
-                drawnow;
-            end
+        if(GFX)
+            animLoop(CUBE,1);
         end
-        fprintf('Zooming (and drawing) took %.4f secs\n',toc);
     end
 end
+
+clear CUBE
+
