3c099e9f230c0562e8be56f49a5e7eb9c7172cd9
Johanan L. Codona <jlcodona@gmail.com>
2017-02-22 10:57:42 -0700

    Added inclusive option to isBetween

--- AOSim2/utils/isBetween.m
+++ AOSim2/utils/isBetween.m
@@ -1,9 +1,20 @@
-function yesno = isBetween(X,a,b)
-% yesno = isBetween(X,a,b)
+function yesno = isBetween(X,a,b,inclusive)
+% yesno = isBetween(X,a,b,[inclusive=false])
 % This is an interior interval check to see if X \in (a,b).
 % a and b can be listed in any order.
 % X can be an array, resulting in a boolean array output.
+% The optional inclusize flag determines if endpoints count.
+
+if(nargin<4)
+    inclusive = false;
+else
+    inclusive = logical(inclusive);
+end
 
 LIMS = sort([a b]);
-yesno = (X>LIMS(1)) & (X<LIMS(2));
 
+if(inclusive)
+    yesno = (X>=LIMS(1)) & (X<=LIMS(2));
+else
+    yesno = (X>LIMS(1)) & (X<LIMS(2));
+end
