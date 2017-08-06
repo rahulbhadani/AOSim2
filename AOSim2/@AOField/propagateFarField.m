1e07a7b7f3e7c98a454ee899eb2019001ae52f0f
Johanan L. Codona <jlcodona@gmail.com>
2017-08-06 13:40:47 -0700

    Reverse Far-Field uses ifft and clear the FFT cache.

--- AOSim2/@AOField/propagateFarField.m
+++ AOSim2/@AOField/propagateFarField.m
@@ -18,7 +18,13 @@ Fin.interpolate_method = 'cubic';
 
 Fin.FFTSize = Fin.size;
 Fin.spacing(F.lambda./F.extent*deltaZ);
-Fin.grid(Fin.fft);
+
+if(F.direction>0)
+    Fin.grid(Fin.clearCache.fft);
+else
+    Fin.grid(Fin.clearCache.ifft);
+end
+
 Fin.scaleTotalPower(POWER0);
 
 % F output grid is different.
@@ -33,7 +39,7 @@ F.zero + Fin;  % This cubic interpolates to the Fout grid.  No spherical wave ye
 
 [X,Y] = F.COORDS;
 SAG = F.dsphere(deltaZ,X,Y);
-F.grid(F.grid.*(-1i*exp(-1i*F.k*SAG)));
+F.grid(F.grid.*(-1i*F.direction*exp(-1i*F.direction*F.k*SAG)));
 
 F.z = F.z + F.direction*deltaZ;
 
