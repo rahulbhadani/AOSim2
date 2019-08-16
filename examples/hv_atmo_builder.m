c10724fab6d91199325e197ba8247435ce07f8e2
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-08-16 01:06:23 -0700

    Set some fixed values for constant

--- examples/hv_atmo_builder.m
+++ examples/hv_atmo_builder.m
@@ -4,13 +4,9 @@
 dx = 0.04;
 PS_SIZE = 2048;
 
-if(~exist('WIND'))
-    WIND = 30;
-end
+WIND = 30;
 
-if(~exist('Cn2_Ground'))
-    Cn2_Ground = 1e-14;
-end
+Cn2_Ground = 1e-14;
 
 LAMBDA = AOField.VBAND;
 
@@ -37,6 +33,7 @@ for n=1:length(HEIGHTS)
     ATMO.addLayer(ps,HEIGHTS(n));
     
     ATMO.layers{n}.Wind = [10 15] + [0 WIND]*HEIGHTS(n)/10000; % Jet Stream.
+    input('Continue ...');
 end
 
 ATMO.show;
